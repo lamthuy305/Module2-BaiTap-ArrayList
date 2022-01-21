b5674889fe0b5a5ff7aaeec6f7e8027e3c5f9f42
lamthuy305 <lamthuy305@gmail.com>
2022-01-21 10:43:42 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,40 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            MyArrayList<String> list = new MyArrayList<>();
+            list.add("One");
+            list.add("Two");
+            list.add("Three");
+            list.add("Four");
+            list.add(0, "Zero");
+            list.add("Three");
+            printMyArrayList(list, "init array list");
+
+            list.replace("One", "Ten");
+            list.replaceAll("Three", "Thirteen");
+            printMyArrayList(list, "replace some elements");
+
+            list.remove(1);
+            list.removeAll("Thirteen");
+            printMyArrayList(list, "remove some elements");
+
+
+            list.clear();
+            printMyArrayList(list, "clear list");
+        }
+
+        public static void printMyArrayList(MyArrayList list, String msg) {
+            System.out.printf("---%s---\n", msg);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+            System.out.println("---------------------");
+        }
+
+        public static void printMyArrayList(MyArrayList list) {
+            printMyArrayList(list, "---------------");
+    }
+}
